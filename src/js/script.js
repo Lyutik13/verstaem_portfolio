8a00cda29f41bb2604db1b7c9830faf073388741
Lyutik13 <lyutikov.viktor.96@mail.ru>
2022-12-16 23:55:26 +0300

    skills__stat %

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+// hamburger
 const hamburger = document.querySelector(".hamburger"),
   menu = document.querySelector(".menu"),
   close = document.querySelector(".menu__close");
@@ -9,3 +10,13 @@ hamburger.addEventListener("click", () => {
 close.addEventListener("click", () => {
   menu.classList.remove("active");
 });
+// hamburger end
+
+// skills__stat %
+var counters = document.querySelectorAll(".skills__stat-counter"),
+  lines = document.querySelectorAll(".skills__stat-line span");
+
+counters.forEach( (item, i) => {
+  lines[i].style.width = item.innerHTML;
+});
+// skills__stat % end
